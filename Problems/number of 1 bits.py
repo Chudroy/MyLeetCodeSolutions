12958c97dfb4eb35a59e5d5b5c5620f498139071
Chudroy <nijotigajo@gmail.com>
2022-06-23 15:10:01 +0200

    longest common prefix solution

--- Problems/number of 1 bits.py
+++ Problems/number of 1 bits.py
@@ -6,5 +6,13 @@ class Solution:
             c += 1
         return c
 
+
 s = Solution()
 print(s.hammingWeight(11111111111111111111111111111101))
+
+print(bin(14))
+print(bin(13))
+
+a = bin(14)
+b = bin(13)
+print(10 )
