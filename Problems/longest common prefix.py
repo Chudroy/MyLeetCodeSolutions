65931c70bea90bad6059c2220f9c047d95965973
Chudroy <nijotigajo@gmail.com>
2022-06-23 15:31:14 +0200

    longest common prefix pythonic

--- Problems/longest common prefix.py
+++ Problems/longest common prefix.py
@@ -3,27 +3,17 @@ from typing import *
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
 
-        if len(strs) < 2:
-            return strs[0]
+        shortest = min(strs, key=len)
 
-        ans = ""
-        for i in range(len(strs) - 1):
-            pointer = 0
-            current_prefix = ""
-            while pointer < len(strs[0]) and pointer < len(strs[i + 1]):
-                if strs[0][pointer] != strs[i + 1][pointer]:
-                    break
-                current_prefix += strs[0][pointer]
-                pointer += 1
-            if not ans:
-                if not current_prefix:
-                    return current_prefix
-                ans = current_prefix
-            elif current_prefix < ans:
-                ans = current_prefix
-        return ans
+        for i, char in enumerate(shortest):
+            for other in strs:
+                if other[i] != char:
+                    return shortest[:i]
+        return shortest
 
 
 s = Solution()
-print(s.longestCommonPrefix(["c", "acc", "ccc"]))
+print(s.longestCommonPrefix(["a", "a", "a"]))
