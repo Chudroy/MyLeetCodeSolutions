3179fe64fb97527b2da23dfbf352619c56ca143f
Chudroy <nijotigajo@gmail.com>
2022-06-27 13:24:45 +0200

    move zeroes

---
+++ Problems/move zeros.py
@@ -0,0 +1,22 @@
+from typing import *
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            if nums[slow] != 0:
+                slow += 1
+
+
+s = Solution()
+print(s.moveZeroes([0, 1, 0, 3, 12, 0, 5, 6, 0]))
